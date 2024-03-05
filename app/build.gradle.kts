dceb2a61d3a69bccd9c7e6af737224d5caf6adc3
HaeTi <t9h9@naver.com>
2024-03-05 16:55:39 +0900

    add orbit dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("dagger.hilt.android.plugin")
     kotlin("kapt") version "1.5.21"
+    id ("kotlin-kapt")
 }
 
 android {
@@ -65,6 +66,17 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.48.1")
     kapt("com.google.dagger:hilt-android-compiler:2.48.1")
     kapt("androidx.hilt:hilt-compiler:1.0.0-alpha03")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+
+    implementation ("androidx.room:room-runtime:2.6.1")
+    kapt ("androidx.room:room-compiler:2.6.1")
+
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+
+    implementation("org.orbit-mvi:orbit-core:4.4.0")
+    implementation("org.orbit-mvi:orbit-viewmodel:4.4.0")
+    implementation("org.orbit-mvi:orbit-compose:4.4.0")
+    testImplementation("org.orbit-mvi:orbit-test:4.4.0")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
